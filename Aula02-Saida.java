80a98b833a845b362efdd93792eab8ced2ca283d
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2025-03-01 19:48:38 -0300

    Create Aula02-Saida.java

---
+++ Aula02-Saida.java
@@ -0,0 +1,23 @@
+package main;
+
+public class App 
+{
+	
+	public static void main(String[]args)
+	
+	{
+	
+	int minhaIdade = 18;
+	
+	String meuNome = "Eli";
+	
+	var idadeSogra = 200;
+	
+	System.out.println("Meu nome " + meuNome);
+	
+	System.out.println("Minha idade " + minhaIdade);
+	
+	System.out.println("Idade sogra " + idadeSogra);
+
+	}
+}
