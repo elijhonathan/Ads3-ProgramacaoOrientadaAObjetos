22ba4ea1c308de524ca25db2e990126f4144a856
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2025-03-01 19:48:19 -0300

    Create Aula01-PrintConcatenado.java

---
+++ Aula01-PrintConcatenado.java
@@ -0,0 +1,20 @@
+package main;
+
+public class ExercicioTres
+{
+	
+	public static void main(String[]args)
+	
+	{
+	
+		String nome = "Jubileu";
+		String cpf = "235.698.745-25";
+		String cep = "85269-950";
+		String cidade = "Quienopolis";
+		String estado = "PR";
+		
+		System.out.println("A pessoa com o nome " + nome + " registrada no cpf " + cpf + ", é residente do cep " + cep + " na cidade " + cidade + "-" + estado );
+				
+
+	}
+}
