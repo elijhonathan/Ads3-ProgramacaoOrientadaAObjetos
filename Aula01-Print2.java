1f76a58164dcd3d72f42c79b0f6eb34a6369cebd
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2025-03-01 19:47:53 -0300

    Create Aula01-Print2.java

---
+++ Aula01-Print2.java
@@ -0,0 +1,21 @@
+package main;
+
+public class SegundoExercicio 
+{
+	
+	public static void main(String[]args)
+	
+	{
+	
+		String nome = "Josefino" ;
+		int idade = 39;
+		int batimento = 120;
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(batimento - idade);
+		
+		
+
+	}
+}
