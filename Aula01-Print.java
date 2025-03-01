5e0725815437423ee63f6b1afa1019a625b8f907
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2025-03-01 19:47:30 -0300

    Create Aula01-Print.java

---
+++ Aula01-Print.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Exercicio 
+{
+	
+	public static void main(String[]args)
+	
+	{
+	
+		char sexo = 'B';
+		String nome = "Clove";
+		int idade = 158;
+		
+		System.out.println(sexo);
+		System.out.println(nome);
+		System.out.println(idade);
+
+	}
+}
