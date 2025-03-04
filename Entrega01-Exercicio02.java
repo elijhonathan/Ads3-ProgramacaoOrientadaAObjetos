75b6036b6ed75b35de0b083ecf9a1bfd65e01729
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2025-03-04 00:13:26 -0300

    Create Entrega01-Exercicio02.java

---
+++ Entrega01-Exercicio02.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Exercicio02 
+{
+	
+	public static void main(String[]args) 
+	{
+
+		double media1 = (8 + 9 + 7) / 3;
+		
+		double media2 = (4 + 5 + 6) / 3;
+		
+		double soma = media1 + media2;
+		
+		double mediaSoma = (media1 + media2) / 2;
+		
+		
+		
+		System.out.println("A media de 8, 9 e 7 é =  " + media1);
+		System.out.println("A media de 4, 5 e 6 é =  " + media2);
+		System.out.println("A soma das duas médias é =  " + soma);
+		System.out.println("A media da soma das médias é =  " + mediaSoma);
+		
+	}
+
+}
